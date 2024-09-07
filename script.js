7e0075d6414b0005aa6093ffea233a83b531937c
seu-username <seu-email>
2024-09-07 18:47:38 -0300

    atualização java

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function numberGeneretion(){
     const max = Math.floor(document.querySelector(".input-max").value)
     const generatedNumber = document.querySelector("span")
 
-
+if(min < max){
     const result = Math.floor(Math.random() * (max - min + 1) + min)
 
     generatedNumber.innerHTML = result
+} else{
+    alert("coloque um número máximo maior que o número mínimo")
+}
 }
